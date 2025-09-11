c7124e171c2aa4877e2c4c462897268808630224
monjurulhaquerajun <monjurulhaquerajun@gmail.com>
2025-09-12 03:04:09 +0600

    conditions

---
+++ 08.conditions.py
@@ -0,0 +1,48 @@
+# If statement:
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+  
+# The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+
+# The else keyword catches anything which isn't caught by the preceding conditions.
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+
+# One line if else statement:
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+#Match expression
+day = 4
+match day:
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
